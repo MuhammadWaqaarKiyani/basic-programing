6b57bf05e30ea89019f805efc8b1925fab15772f
MuhammadWaqaarKiyani <muhammadwaqarkiyani89@gmail.com>
2025-05-17 07:39:43 -0700

    Update float value.cpp

--- float value.cpp
+++ float value.cpp
@@ -6,7 +6,7 @@ int main() {
     printf("Enter Four Values:\n");
     scanf("%f %f %f %f", &value1, &value2, &value3, &value4);
 
-    avg = (value1 + value2 + value3 + value4) / 4;
+    avg = (value1 + value2 + value3 + value4) / 4; 
 
     printf("The Average of %.2f, %.2f, %.2f and %.2f is: %.2f\n", value1, value2, value3, value4, avg); 
 
